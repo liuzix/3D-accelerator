b282abccf352bb3e88d53f052766ec56fd6eb3ef
kuroiiwa <zhangkaige96@gmail.com>
2019-04-09 12:55:07 -0700

    fifo test

---
+++ test/main_test.cpp
@@ -0,0 +1,63 @@
+#include "Vfifo.h"      // From Verilating "top.v"
+#include <iostream>    // Need std::cout
+#include <verilated.h> // Defines common routines
+#include <string.h>
+
+using namespace std;
+Vfifo *top; // Instantiation of module
+
+vluint64_t main_time = 0; // Current simulation time
+// This is a 64-bit integer to reduce wrap over issues and
+// allow modulus.  You can also use a double, if you wish.
+
+double sc_time_stamp() { // Called by $time in Verilog
+  return main_time;      // converts to double, to match
+                         // what SystemC does
+}
+
+void feed(Vfifo *top, bool wr, bool rd, int* din) {
+    top->wr = wr;
+    top->rd = rd;
+    memcpy(top->din, din, 3 * sizeof(int));
+}
+
+int main(int argc, char **argv) {
+  Verilated::commandArgs(argc, argv); // Remember args
+
+  top = new Vfifo; // Create instance
+
+  top->clk = 0;
+  top->reset = 1; // Set some inputs
+  top->eval();
+  top->reset = 0;
+  top->eval();
+  top->reset = 1;
+
+  int i[3] = {0,0,0};
+  while (!Verilated::gotFinish()) {
+    feed(top, 1, 0, i);
+    i[0] += 1;
+    i[1] += 2;
+    i[2] += 3;
+    top->clk = 1;
+    if (!top->empty)
+        top->rd = 1;        
+    else{
+        top->rd = 0;
+        cout << "empty!" << endl;
+    }
+    top->eval();
+
+    cout << top->dout[0] << endl; // Read a output
+    cout << top->dout[1] << endl; // Read a output
+    cout << top->dout[2] << endl; // Read a output
+ 
+ 
+    top->clk = 0;
+    top->eval();
+  }
+
+  top->final(); // Done simulating
+  //    // (Though this example doesn't get here)
+  delete top;
+}

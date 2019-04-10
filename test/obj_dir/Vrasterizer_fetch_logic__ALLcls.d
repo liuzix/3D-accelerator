7e308f1451e4fd5b12166c227bf061250fdd563e
Chao Lin <chaolinle@gmail.com>
2019-04-10 17:32:04 -0400

    removed files

--- test/obj_dir/Vrasterizer_fetch_logic__ALLcls.d
+++
@@ -1,6 +0,0 @@
-Vrasterizer_fetch_logic__ALLcls.o: Vrasterizer_fetch_logic__ALLcls.cpp \
-  Vrasterizer_fetch_logic.cpp Vrasterizer_fetch_logic.h \
-  /usr/local/Cellar/verilator/4.010/share/verilator/include/verilated_heavy.h \
-  /usr/local/Cellar/verilator/4.010/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.010/share/verilator/include/verilatedos.h \
-  Vrasterizer_fetch_logic__Syms.h

29e422e33feeda7aaaf53838c23129be9a2b71a7
jiehuang239 <46855687+jiehuang239@users.noreply.github.com>
2019-04-12 15:57:56 -0400

    new commit

--- bus_adapter.sv
+++ bus_adapter.sv
@@ -1,14 +1,14 @@
-module bus_adapter #(parameter N=8)(
-    input clk,
+module bus_adapter (
+        input clk,
 	input reset,
 	//connect to upper master
 	output logic valid,
 	output logic wait,
-	input logic write,
-	input logic read,
-	inout logic [31:0]data,
-	input logic[25:0]address,
-	input logic [N-1:0]burst_num,
+	input logic write,//write enable signal
+	input logic read,//read enable signal
+	input logic [31:0]write_data,
+        output logic [31:0] read_data,
+	input logic[25:0]address,	
 	//connect to sram controller 
 	//read from sram
 	input [15:0] master_readdata,
@@ -20,93 +20,111 @@ module bus_adapter #(parameter N=8)(
 	output master_write,
 	output [1:0] master_byteenable,	
 	output [15:0]master_writedata);// write to sram
-	
-//pointer of fifo queue
-logic [7:0]head;
-logic[7:0]tail;
-logic [15:0]queue[2**N-1:0];//default maximum burst is 256
-//three states: IDLE, read data, write data
+//three states: IDLE, read data, write data,WAIT
 parameter IDLE=2'b00;
 parameter READ_DATA=2'b01;
 parameter WRITE_DATA=2'b10;
 parameter WAIT=2'b11;
 logic[1:0] state,
 logic[1:0] nextstate,
+logic flag;
+logic readflag;
 //state register
 always_ff@(posedge clk or posedge rst)
 begin
-  if(rst)state<=init;
+  if(rst)state<=IDLE;
    else state<=nextstate;
   
 end
 //next_state logic
-logic burst_done;
+
 always_comb
 begin
    case(state)
-	  WAIT:nextstate=IDLE;
-	  IDLE: begin if(write)nextstate=WRITE_DATA;//if write=1, write data to sram
-	        else if(read)nextstate=READ_DATA;
+	  WAIT:begin
+                if(master_waitrequest==0)nextstate=IDLE;
+                else nextstate=WAIT;
+               end
+	  IDLE: begin 
+                if(master_waitrequest)nextstate=WAIT;
+                else begin
+                  if(write)nextstate=WRITE_DATA;//if write=1, write data to sram
+	          else if(read)nextstate=READ_DATA;
 	        end
-	  WRITE_DATA:begin 
-	        if(burst_done)nextstate=IDLE;
-	        else nextstate=WRITE_DATA;
+                end
+	  WRITE_DATA:begin 	       
+                if(master_waitrequest)next_state=WAIT;
+	        else begin 
+                if(write|flag==1)nextstate=WRITE;
+                else if(write==0&&read==1)nextstate=READ;
+                else if(write==0&&read==0)nextstate=IDLE;
+                end
 	        end
-	  READ_DATA:begin if(burst_done)nextstate=IDLE;
-	        else nextstate=READ_DATA;
+	  READ_DATA:begin 
+                if(master_waitrequest)next_state=WAIT;
+	        else begin 
+                  if(read)nextstate=READ;
+                  else if(write==1&&read==0)nextstate=WRITE;
+                  else if(write==0&&read==0)nextstate=IDLE;
+                end
 	        end
 	endcase
 end
 //computation in each state
-int endofqueue;
-int i;
-int j;
-logic flag;
+logic[31:0]buffer[2:0];
+logic[25:0] addressbuffer;
+
+logic read_request_flag;
 always_ff@(posedge clk)
 begin
    case(state)
 	  S3: begin end
 	  IDLE: begin 
-	           burst_done<=0;
-	           head<=0;//head of the queue
-	           tail<=0;
-	           i<=0;
-	           j<=0;
-	           endofqueue=2**N;
-	           flag<=0;
-			  end
+	          flag<=0;
+                  readflag<=0;
+	        end
 	  WRITE_DATA:begin
 	                master_read<=0;
-					master_write<=1;	               
-				    for(i;i<burst_num;i++)begin
-				       queue[tail]<=data[31:16];
-				       queue[tail+1]<=data[15:0];
-				       tail<=tail+2;
-				       master_address<=address+i;
-				       if(i==0)master_writedata<=data[31:16];
-				       else begin
-				           master_writedata<=queue[head];
-				           head<=head+1;
-				          end
-				        if(tail>=endofqueue)tail<=0;
-				        if(head>=endofqueue)head<=0;
-				    end
-				    if(i>=burst_num)burst_done<=1;
+                        master_write<=1;
+                        buffer[31:0]<=write_data[31:0];
+                        wait<=1;
+                        flag<=1;
+                        master_writedata[15:0]<=write_data[31:16];
+                        master_address<=address;
+                        if(read==1)begin
+                           addressbuffer<=address;
+                           read_request_flag<=1;
+                        end
+                        if(flag==1)begin 
+                           master_writedata[15:0]<=buffer[15:0];
+                           flag<=0;
+                           wait<=0;
+                           master_address<=master_address+1;
+                           end
 	              end
 	  READ_DATA:begin
 	              master_read<=1;
-	              master_write<=0;
-	              valid<=1;
-	              for(j;j<2*burst_num;j++)begin
-	                  if(flag==0)queue[0]<=master_writedata[15:0];
-	                  else data={queue[0],master_writedata[15:0]};
-	                  flag=!flag;
-	                end
-				 end
+                      master_write<=0;
+                      wait<=1;
+                      master_address<=address;//send data to sram controller
+                      
+                        
+		    end
 	endcase
 end
-
-
-
-
+//go up
+logic [31:0]read_data_buffer;
+always_ff@(posedge clk)
+begin
+  if(master_readdatavalid)
+     if(readflag==0)begin
+       read_data_buffer[31:16]<=master_readdata[15:0];
+       readflag=!readflag;
+    end
+    else begin
+       read_data[31:0]<={read_data_buffer[31:16],master_readdata[15:0]};
+       readflag=!readflag;
+    end
+     
+end
 endmodule

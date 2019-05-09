2e9865b3f138cd9d6f87170dcddebec1f261650b
kuroiiwa <zhangkaige96@gmail.com>
2019-05-09 15:45:20 -0400

    fetcher for pipeline protocol

--- rasterizer_vertex_fetch.sv
+++ rasterizer_vertex_fetch.sv
@@ -23,39 +23,93 @@ module rasterizer_vertex_fetch (
     output [31:0] vertex_out[14:0]
 );
 
+    parameter FIFO_SIZE = 4;
+
     typedef enum logic[1:0] {IDLE_S, SEND, TRI_SEND} send_t;
     send_t send_state;
 
     typedef enum logic[1:0] {IDLE_R, FETCH, TRI_FETCH} rec_t;
     rec_t rec_state;
 
-
+    logic [31:0] vertex_out_buf[14:0];
+    logic [3:0] r_count;
+    int output_count;
     logic [25:0] addr;
     int tri_num = 0;
-    int total_count;
+    int input_count;
 
     assign master_byteenable = 4'b1111;
 
     assign done_out = (tri_num != 0) && (output_count == tri_num);
 
     logic [3:0] s_count;
+
+    logic [479:0] data_in;
+    logic [479:0] data_out;
+    logic wrreq;
+    logic rdreq;
+    logic full;
+    logic empty;
+    logic almost_full;
+    logic almost_empty;
+
+    logic recv_valid;
+    int fifo_size = 2**FIFO_SIZE;
+    int fifo_counter;
+
+    fifo #(.DBITS(480), .SIZE(FIFO_SIZE))fifo(
+        .din(data_in),
+        .dout(data_out),
+        .wr(wrreq),
+        .rd(rdreq),
+        .reset(reset),
+        .clk(clock),
+        .full(full),
+        .empty(empty),
+        .almost_full(almost_full),
+        .almost_empty(almost_empty)
+    );
+
+    assign vertex_out = {>>{data_out}};
+
+    always_ff @(posedge clock or negedge reset) begin
+        if (!reset) begin
+            fifo_counter = 0;
+        end
+
+        if (recv_valid && !full) begin
+            wrreq <= 1;
+            data_in <= {>>{vertex_out_buf}};
+        end else
+            wrreq <= 0;
+
+        if (!stall_in && !empty) begin
+            rdreq <= 1;
+            fifo_counter = fifo_counter - 1;
+            output_valid <= 1;
+        end else begin
+            rdreq <= 0;
+            output_valid <= 0;
+        end
+    end
+
     always_ff @(posedge clock or negedge reset) begin
         if (!reset) begin
             send_state <= IDLE_S;
             master_read <= 0;
             s_count <= 0;
             tri_num = 0;
-            total_count <= 0;
+            input_count <= 0;
         end
         else begin
             case(send_state)
                 IDLE_S: begin
-                    $display("triangles: [%d] addr: [%d]",tri_num, addr);
-                    if (total_count < tri_num && !stall_in) begin
+                    if (input_count < tri_num && fifo_counter < fifo_size) begin
                         master_address <= addr;
                         master_read <= 1;
                         addr <= addr + 4;
-                        total_count <= total_count + 1;
+                        input_count <= input_count + 1;
+                        fifo_counter = fifo_counter + 1;
                         s_count <= s_count + 1;
                         send_state <= SEND;
                     end
@@ -102,20 +156,17 @@ module rasterizer_vertex_fetch (
         end
     end
 
-    logic [31:0] vertex_out_buf[14:0];
-    logic [3:0] r_count;
-    int output_count;
     always_ff @(posedge clock or negedge reset) begin
         if (!reset) begin
             rec_state <= IDLE_R;
-            output_valid <= 0;
+            recv_valid <= 0;
             r_count <= 0;
             output_count <= 0;
         end
         else begin
             case (rec_state)
                 IDLE_R: begin
-                    output_valid <= 0;
+                    recv_valid <= 0;
                     if (master_readdatavalid && tri_num != 0) begin
                         vertex_out_buf[r_count] <= master_readdata;
                         r_count <= r_count + 1;
@@ -130,9 +181,9 @@ module rasterizer_vertex_fetch (
                     if (master_readdatavalid) begin
                         if (r_count == 14) begin
                             r_count <= 0;
-                            vertex_out[13:0] <= vertex_out_buf[13:0];
+                            //vertex_out[13:0] <= vertex_out_buf[13:0];
                             vertex_out[r_count] <= master_readdata;
-                            output_valid <= 1;
+                            recv_valid <= 1;
                             output_count <= output_count + 1;
                             rec_state <= IDLE_R;
                         end else begin
